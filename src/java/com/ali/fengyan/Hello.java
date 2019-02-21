944a0bf44f83853f57822285a6e35bab9e64dc6f
liuyien <513062230@qq.com>
2019-02-21 13:16:00 +0800

    对对对

--- src/java/com/ali/fengyan/Hello.java
+++ src/java/com/ali/fengyan/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("999");
         System.out.println("666");
+        System.out.println("8888");
     }
 }
